7815802113dfa4d56de215f6ac54f076f2fb8228
ritishadhikari <ritishadhikari@gmail.com>
2022-06-27 13:55:10 +0530

    Till 27-06-2022

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-value=[(0,1),(0,1),(0,2),(0,3),(0,3)]
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+        return max(nums)
 
-print(set(value))
+
+if __name__=="__main__":
+    nums = [-1]
+    print(Solution().maxSubArray(nums=nums))
