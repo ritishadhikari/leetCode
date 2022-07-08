980fa1c3e02b92e7882d1d074d97a9465ae5495a
Ritish Adhikari <ritishadhikari@gmail.com>
2022-07-09 00:38:30 +0530

    Till 09-07-2022

---
+++ 392_Is_Subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        if len(s) > len(t):
+            return False
+        if len(s) == 0:
+            return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence == len(s):
+                break
+            else:
+                if s[subsequence]==t[i]:
+                    subsequence+=1
+        return  subsequence == len(s) 
+
+if __name__=="__main__":
+    s="aaaaaa"
+    t="bbaaaa"
+    print(Solution().isSubsequence(s=s,t=t))
