c638c1550b921ee33573541c0ff4c6473c4f821f
Ritish Adhikari <ritishadhikari@gmail.com>
2022-07-09 12:13:03 +0530

    Till 09-07-2022

--- 392_Is_Subsequence.py
+++ 392_Is_Subsequence.py
@@ -1,18 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-
-        if len(s) > len(t):
-            return False
-        if len(s) == 0:
-            return True
-        subsequence=0
-        for i in range(0,len(t)):
-            if subsequence == len(s):
+        pointerAtS=0
+        for l in t:
+            if pointerAtS==len(s):
                 break
-            else:
-                if s[subsequence]==t[i]:
-                    subsequence+=1
-        return  subsequence == len(s) 
+            elif s[pointerAtS]==l:
+                pointerAtS+=1
+        return pointerAtS==len(s) 
 
 if __name__=="__main__":
     s="aaaaaa"
