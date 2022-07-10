dd405ac5aa8857d4e5127c348d084814b592651f
Ritish Adhikari <ritishadhikari@gmail.com>
2022-07-11 01:12:52 +0530

    Till 10-07-2022

--- 264_Ugly_Number_II.py
+++ 264_Ugly_Number_II.py
@@ -3,23 +3,22 @@ from numpy import number
 
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        mul2=0
-        mul3=0
-        mul5=0
+        indMul2=0
+        indMul3=0
+        indMul5=0
         m=[]
         for i in range(n):
             if not m:
                 m.append(1)
             else:
-                nextMin=min(m[mul2]*2,m[mul3]*3,m[mul5]*5)
+                nextMin=min(m[indMul2]*2,m[indMul3]*3,m[indMul5]*5)
                 m.append(nextMin)
-                if m[mul2]*2==nextMin:
-                    mul2+=1
-                if m[mul3]*3==nextMin:
-                    mul3+=1
-                if m[mul5]*5==nextMin:
-                    mul5+=1
-
+                if m[indMul2]*2==nextMin:
+                    indMul2+=1
+                if m[indMul3]*3==nextMin:
+                    indMul3+=1
+                if m[indMul5]*5==nextMin:
+                    indMul5+=1
         return m[-1]
 
 if __name__=="__main__":
