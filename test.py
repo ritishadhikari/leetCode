154735bbf38862431d2d71ed5051ecc1e1696907
Ritish Adhikari <ritishadhikari@gmail.com>
2022-07-15 20:50:04 +0530

    Till 15-07-2022

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-candidate= [
-            [1,2, 3] ,
-            [4, 5, 6]
-        ]
+import os
 
-print(candidate[1][2])
-'''
-[1,2,3,4,5,6,7,8,9]
-
-0th row to 2nd row
-0th col to 2nd col
-
-row ->
-0 to 0+3*2= [1,2,3,4,5,6]
-
-
-
-'''
+print(len(os.listdir()))
