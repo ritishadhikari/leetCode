4f9dcf7d8ec75d95fb9d1c5c70b48e93e0bc40b7
Ritish Adhikari <ritishadhikari@gmail.com>
2022-07-20 05:02:29 +0530

    Till 19-07-2022

--- 221_Maximal_Square.py
+++ 221_Maximal_Square.py
@@ -2,72 +2,40 @@ from typing import List
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
 
-        if len(matrix)==1 and len(matrix[0])==1:
-            if matrix[0][0]=="1":
-                return 1 
-            else:
-                return 0
-
-        elif (len(matrix)>1 and len(matrix[0])==1) or (len(matrix)==1 and len(matrix[0])>1):
-            for arr in matrix:
-                for j in arr:
-                    if j=="1":
-                        return 1
-            return 0
-        else: 
-            prevList=[]
-            oneOffCase=0
-            for i in range(len(matrix[0])-1):
-                value=int(matrix[0][i])
-                right=int(matrix[0][i+1])
-                down=int(matrix[1][i])
-                diagRight=int(matrix[1][i+1])
-
-                if value and right and down and diagRight:
-                    prevList.append(1)
-                else: 
-                    prevList.append(0)
-
-                if value or right or down or diagRight:
-                    oneOffCase=1          
-            maxVal=max(prevList)
-            
-            for i in range(1,len(matrix)-1):
-                presentList=[]
-                for j in range(len(matrix[0])-1):
-                    value=int(matrix[i][j])
-                    top=prevList[j]
-                    if not j:
-                        left=0
-                        diagLeft=0
+        prevList=[int(val) for val in matrix[0]]
+        maxVal=max(prevList)
+        for i in range(1,len(matrix)):
+            presentList=[]
+            for j in range(len(matrix[0])):
+                if not j:
+                    maxVal=max(maxVal,int(matrix[i][j]))
+                    presentList.append(int(matrix[i][j]))
+                else:
+                    if not int(matrix[i][j]):
+                        presentList.append(0)
                     else:
                         left=presentList[j-1]
+                        up=prevList[j]
                         diagLeft=prevList[j-1]
-                    right=int(matrix[i][j+1])
-                    down=int(matrix[i+1][j])
-                    diagRight=int(matrix[i+1][j+1])
-                    if value and right and down and diagRight:
-                        minVal=min(top,left,diagLeft)
-                        maxVal=max(maxVal,minVal+1)
-                        presentList.append(minVal+1)
-                    else:
-                        presentList.append(0)
-
-                    if value or right or down or diagRight:
-                        oneOffCase=1
-                prevList=presentList
-            
-
-            if  maxVal:
-                return (maxVal+1)**2
-            else: 
-                return oneOffCase
+                        if left and up and diagLeft:
+                            if left==up and up==diagLeft:
+                                maxVal=max(maxVal,left+1)
+                                presentList.append(left+1)
+                            else:
+                                maxVal=max(maxVal,min(left,up,diagLeft)+1)
+                                presentList.append(min(left,up,diagLeft)+1)
+                        else:
+                            maxVal=max(maxVal,1)
+                            presentList.append(1)
+            prevList=presentList
+        return maxVal**2
         
 
 
 if __name__=="__main__":
     mat= [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
     # mat=[[1,1,0,1,0],[1,1,1,1,1],[1,1,1,1,1],[0,1,1,1,1],[0,1,1,1,1],[1,0,1,0,1]]
-    mat=[["0","0","0"],["0","0","0"],["1","1","1"]]
-    mat=[["0","1"],["1","0"]]
+    # mat=[["0","0","0"],["0","0","0"],["1","1","1"]]
+    # mat=[["0","1"],["1","0"]]
+    mat=[["1","1","1","1","0"],["1","1","1","1","0"],["1","1","1","1","1"],["1","1","1","1","1"],["0","0","1","1","1"]]
     print(Solution().maximalSquare(matrix=mat))
