2c00d29a8eba8897fa729c24d6255622b53fa865
ritishadhikari <ritishadhikari@gmail.com>
2022-07-27 18:11:39 +0530

    Till 27-07-2022

---
+++ 322_Coin_Change.py
@@ -0,0 +1,18 @@
+from typing import List
+from math import inf
+from typing import List
+class Solution:
+    def coinChange(self, coins: List[int], amount: 'int') -> 'int':
+        evalList=[0]+ [inf for i in range(amount)]
+        for i in range(1,len(evalList)):
+            for coin in coins:
+                if i>=coin:
+                    evalList[i]=min(evalList[i],evalList[i-coin]+1)
+        if evalList[-1]==inf:
+            return -1
+        else:
+            return evalList[-1]
+if __name__=="__main__":
+    coins = [5,3]
+    amount = 10
+    print(Solution().coinChange(coins=coins,amount=amount))
