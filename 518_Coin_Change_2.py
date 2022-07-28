1d7379ccd8212d1ea054c11937a2397fe4e239d9
ritishadhikari <ritishadhikari@gmail.com>
2022-07-28 16:18:22 +0530

    Till 28-07-2022

---
+++ 518_Coin_Change_2.py
@@ -0,0 +1,17 @@
+# VIP
+from typing import List
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp=[1]+[0 for i in range(amount)]
+
+        for i in coins:
+            for j in range(i,len(dp)):
+                dp[j]=dp[j]+dp[j-i]
+
+        return dp[amount]
+
+if __name__=="__main__":
+    amount = 5
+    coins = [1,2,5]
+    print(Solution().change(amount=amount,coins=coins))
