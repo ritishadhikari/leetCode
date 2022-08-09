ef7f34ad73f0ab779def19dfa271494fc477d2a3
Ritish Adhikari <ritishadhikari@gmail.com>
2022-08-09 09:52:39 +0530

    Till 09-08-2022

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -3,16 +3,16 @@ from typing import List
 class Solution:
     def search(self,nums:List[int], target:int):
         l=0
-        r=len(nums)-1
+        r=len(nums)-1  # First Thing to keep note
 
-        while(l<=r):
-            mid=(l+r)//2
+        while(l<=r):  # Second Thing to keep note
+            mid=(l+r)//2  # Third Thing to keep note
             if nums[mid]==target:
-                return mid
+                return mid  # Fourth thing to keep note
             elif nums[mid]<target:
-                l=mid+1
+                l=mid+1  # Fifth thing to keep note
             else:
-                r=mid-1
+                r=mid-1  # Sixth thing to keep note
         return -1
 
 
