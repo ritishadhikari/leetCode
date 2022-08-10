7db3e038582283c506c276a828c005d787b3a7e4
Ritish Adhikari <ritishadhikari@gmail.com>
2022-08-10 16:09:40 +0530

    Till 10-08-2022

--- 438_Find_All_Anagrams_in_a_String.py
+++ 438_Find_All_Anagrams_in_a_String.py
@@ -1,5 +1,6 @@
-from typing import DefaultDict, List
-from collections import Counter, defaultdict
+# VVI
+from typing import  List
+from collections import defaultdict
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
@@ -13,28 +14,22 @@ class Solution:
         #         lst.append(i)
 
         # return lst
-      
-        pDict=DefaultDict(lambda: 0)
-        for let in p:
-            pDict[let]+=1
 
-        lenP=len(p)
-        lenS=len(s)
+        lenP,lenS= len(p),len(s)
+        pDict=defaultdict(lambda: 0)
+        for let in p: pDict[let]+=1
+        sDict=defaultdict(lambda:0)
+        for let in s[:lenP-1]: sDict[let]+=1
         lst=[]
-        sDict=DefaultDict(lambda:0)
-        for let in s[:lenP-1]:
-            sDict[let]+=1
         maxRange=min(lenS,lenS-lenP+1)
         for i in range(maxRange):
             endPoint=min(lenP,(lenS-i))-1
             sDict[s[i+endPoint]]+=1
 
-            if sDict==pDict:
-                lst.append(i)
+            if sDict==pDict: lst.append(i)
             sDict[s[i]]-=1
 
-            if sDict[s[i]]==0:
-                del sDict[s[i]]
+            if sDict[s[i]]==0: del sDict[s[i]]
         return lst
 
 
