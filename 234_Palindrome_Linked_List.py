fce88a79e1b7ceb0a82423ea104b72ffaea51df6
ritishadhikari <ritishadhikari@gmail.com>
2022-08-25 02:02:55 +0530

    Till 25-08-2022

---
+++ 234_Palindrome_Linked_List.py
@@ -0,0 +1,17 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        pcheck=''
+        while head:
+            pcheck+=str(head.val)
+            head=head.next
+        return True if pcheck[::-1]==pcheck else False
+
+head=ListNode(1,ListNode(2,ListNode(3,ListNode(1,None))))
+
+print(Solution().isPalindrome(head=head))
