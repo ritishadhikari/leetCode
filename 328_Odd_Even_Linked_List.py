910843a7f9e2c587325f3d0deba6effc53f9bb0c
ritishadhikari <ritishadhikari@gmail.com>
2022-08-28 21:06:47 +0530

    Till 28-08-2022

--- 328_Odd_Even_Linked_List.py
+++ 328_Odd_Even_Linked_List.py
@@ -38,11 +38,6 @@ class Solution:
             return actualOdd
         else:
             return None
-                
-
-
-
-
 
 if __name__=="__main__":
     head=ListNode(val=1,next=ListNode(val=2,next=ListNode(val=3,next=ListNode(val=4,next=None))))
