af611ecb1a953839413237658755ccea6bd208ad
ritishadhikari <ritishadhikari@gmail.com>
2022-09-07 09:29:24 +0530

    Till 07-09-2022

---
+++ 230_Kth_Smallest_Element_in_a_BST.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        count=0
+        # seenleftNode=set()
+        stack=[root]
+        last=None
+        while stack:
+            node=stack[-1]
+            if node.left and node.left!=last :
+                stack.append(node.left)
+            else:
+                node=stack.pop()
+                count+=1
+                if count==k:
+                    return node.val
+                if node.right:
+                    stack.append(node.right)
+                last=node
+
+if __name__=="__main__":
+    root=TreeNode(val=5,
+                left=TreeNode(val=3,
+                            left=TreeNode(val=2,
+                                        left=TreeNode(val=1,
+                                                    left=None,
+                                                    right=None),
+                                        right=None),
+                            right=TreeNode(val=4,
+                                        left=None,
+                                        right=None)),
+                right=TreeNode(val=6,
+                            left=None,
+                            right=None))
+
+    print(Solution().kthSmallest(root=root,k=3))
