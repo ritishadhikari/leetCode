cff7b674766c92a91c3249271123e8f44c39f328
Ritish Adhikari <ritishadhikari@gmail.com>
2022-09-21 10:24:27 +0530

    Till 09/21/2022 10:24:27

---
+++ 208_Implement_Trie_(Prefix Tree).py
@@ -0,0 +1,34 @@
+class Trie:
+
+    def __init__(self):
+        self.trie={}
+
+    def insert(self, word: str) -> None:
+        trie=self.trie
+        for w in word:
+            trie=trie.setdefault(w,{})
+        trie["word"]=word
+
+    def search(self, word: str) -> bool:
+        trie=self.trie
+        for w in word:
+            if w not in trie: return False
+            else: trie=trie[w]
+        return True if 'word' in trie else False
+
+        
+    def startsWith(self, prefix: str) -> bool:
+        trie=self.trie
+        for w in prefix:
+            if w not in trie: return False
+            else: trie=trie[w]
+        return True
+
+if __name__=="__main__":
+    objTrie=Trie()
+    objTrie.insert(word="apple")
+    objTrie.insert(word="app")
+    objTrie.insert(word="monster")
+    print(objTrie.search(word="app"))
+    print(objTrie.search(word="mon"))
+    print(objTrie.startsWith(prefix="mon"))
--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-o=set()
+class Trie(object):
 
-o.add(4)
-o.add(-3)
-o.add(6)
+    def __init__(self):
+        self.trie = {}
 
-print(o)
+    def insert(self, word):
+        trie = self.trie
+        for c in word:
+            trie = trie.setdefault(c, {})
+        trie['word'] = word
+        s=2
+    def search(self,word):
+        trie=self.trie
+        for c in word:
+            if c not in trie:
+                return False
+            trie=trie[c]
+        if 'word' in trie:
+            return True
+        else:
+            return False
+
+trieObj=Trie()
+trieObj.insert(word="apple")
+trieObj.insert(word="app")
+print(trieObj.search(word="apple"))
