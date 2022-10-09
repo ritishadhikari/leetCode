4ead08521cbbe421198ae44be4370035ca58034b
Ritish Adhikari <ritishadhikari@gmail.com>
2022-10-09 20:32:07 +0530

    Till 10/09/2022 20:32:07

--- 200_Number_of_Islands.py
+++ 200_Number_of_Islands.py
@@ -6,29 +6,26 @@ class Solution:
         col=len(grid[0])
         count=0
         traceDict=defaultdict(lambda : [])
-        up,down,left,right=[True]*4
         for i in range(row):
             for j in range(col):
+                up,down,left,right=[False]*4
                 if int(grid[i][j]):
                     if i<row-1 and int(grid[i+1][j]):
                         traceDict[(i,j)].append((i+1,j))
+                        down=True
                     if j<col-1 and int(grid[i][j+1]):
                         traceDict[(i,j)].append((i,j+1))
+                        right=True
                     if j and int(grid[i][j-1]):
                         traceDict[(i,j)].append((i,j-1))
+                        left=True
                     if i and int(grid[i-1][j]):
                         traceDict[(i,j)].append((i-1,j))  
+                        up=True
                     # When all the Adjacent are Zero
-                    if (i and not int(grid[i-1][j])) or not i:
-                        up=False
-                    if (j and not int(grid[i][j-1])) or not j:
-                        left=False
-                    if (j<col-1 and not int(grid[i][j+1])) or j==col-1:
-                        right=False
-                    if (i<row-1 and not int(grid[i+1][j])) or i==row-1:
-                        down=False
-                    if not down and not up and not right and not left:
-                        traceDict[(i,j)].append(None) 
+                    if left+right+up+down==0:
+                        count+=1
+                   
         stack=[]
         for i in range(row):
             for j in range(col):
@@ -44,5 +41,8 @@ class Solution:
 
         return count
 if __name__=="__main__":
-    grid = [["0","0","0","0","0","0","1"]]
+    grid = [["1","1","0","0","0"],
+            ["1","1","0","0","0"],
+            ["0","0","1","0","0"],
+            ["0","0","0","1","1"]]
     print(Solution().numIslands(grid=grid))
