4fdcc7227acaa6d3bc44a9048ef66ee39d0eee40
ritishadhikari <ritishadhikari@gmail.com>
2022-10-27 23:49:32 +0530

    Till 10/27/2022 23:49:32

--- 1557_Minimum_Number_of_Vertices_to_Reach_All_Nodes.py
+++ 1557_Minimum_Number_of_Vertices_to_Reach_All_Nodes.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        
         traceDict={i:0 for i in range(n)}
-
         for s,d in edges:
             traceDict[d]+=1
-        
         out=[l for l in traceDict if traceDict[l]==0]
+        
         return out
 if __name__=="__main__":
     n =5
