403067e7d85544d5f3caa36e8f36e13058af70c1
Ritish Adhikari <ritishadhikari@gmail.com>
2022-11-30 18:38:47 +0530

    Till 11/30/2022 18:38:47

---
+++ 633_Sum_of_Square_Numbers.py
@@ -0,0 +1,23 @@
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a=math.sqrt(c)
+        if int(a)**2==c:
+            return True
+        else:
+            a=int(a)
+        rem=int(c-(a**2))
+
+        b=math.sqrt(rem)
+        while a>=b:
+            
+            if rem%b==0:
+                return True
+            else:
+                a-=1
+                rem=int(c-(a**2))
+                b=math.sqrt(rem)
+
+if __name__=="__main__":
+    c=1000
+    print(Solution().judgeSquareSum(c=c))
