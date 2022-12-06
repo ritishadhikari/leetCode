28c99858d78727dca931c9ecf5e7ff5cb095b372
Ritish Adhikari <ritishadhikari@gmail.com>
2022-12-06 09:04:24 +0530

    Till 12/06/2022 09:04:24

--- 33_Search_in_Rotated_Sorted_Array.py
+++ 33_Search_in_Rotated_Sorted_Array.py
@@ -14,29 +14,24 @@ class Solution:
                 if target>nums[m]:
                     if sl and sr or sl and not sr:  # When Number is/may part of unsorted right from mid
                         l=m+1
-                        m=(r+l)//2
                     elif not sl and sr:
                         if nums[r]>=target:  # When Number is/may part of sorted right from mid
                             l=m+1
-                            m=(r+l)//2
                         else:  # When Number is/may part of unsorted Left from Mid
                             r=m-1
-                            m=(r+l)//2
                 else:
                     if sl and sr or sr and not sl:  # When Number is/may part of unsorted left from mid
                         r=m-1
-                        m=(r+l)//2
                     elif not sr and sl:
                         if nums[l]<=target:  # When Number is/may part of sorted left from mid
                             r=m-1
-                            m=(r+l)//2
                         else:   # When Number is/may part of unsorted right from Mid
                             l=m+1
-                            m=(r+l)//2
-        return -1    
+                m=(r+l)//2
+        return -1 
 
 
 if __name__=="__main__":
-    nums = [1]
-    target=0
+    nums = [89,91,102,9,1,3,4,5,6]
+    target=1
     print(Solution().search(nums=nums,target=target))
