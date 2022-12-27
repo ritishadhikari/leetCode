3f175e37928cc1ae88eefa7e50b4f0c3d8ee8a30
Ritish Adhikari <ritishadhikari@gmail.com>
2022-12-27 18:58:11 +0530

    Till 12/27/2022 18:58:11

---
+++ 875_Koko_Eating_Bananas.py
@@ -0,0 +1,28 @@
+from typing import List
+from math import ceil
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def valid(guess):
+            count=0
+            for p in piles:
+                count+=ceil(p/guess)
+                if count>h: return False
+            return True
+
+        l=1
+        r=max(piles)
+        most=r
+
+        while l<=r:
+            m=(l+r)//2
+            if valid(guess=m):
+                r=m-1
+                most=min(m,most)
+            else:
+                l=m+1
+        return most
+
+if __name__=="__main__":
+    piles = [1000000000,1000000000]
+    h=3
+    print(Solution().minEatingSpeed(piles=piles, h=h))
