0ffb9cdec1d5118e9cd1a501d3769c23c1dc18c2
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-05 21:50:16 +0530

    Till 01/05/2023 21:50:15

---
+++ 1818_Minimum_Absolute_Sum_Difference.py
@@ -0,0 +1,29 @@
+# VVI
+from bisect import bisect_left
+from typing import List
+from math import inf
+
+class Solution:
+    def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
+        totalDiff=0
+        diffList=[]
+        mod=10**9 + 7
+        best=inf
+        n=len(nums1)
+        
+        for i in range(n):
+            diff=abs(nums1[i]-nums2[i])
+            totalDiff+=diff
+            diffList.append(diff)
+        nums1.sort()
+
+        for i,num in enumerate(diffList):
+            idx=bisect_left(a=nums1,x=nums2[i])
+            if idx>0: best=min(best, totalDiff-num+abs(nums2[i]-nums1[idx-1]))
+            if idx<n: best=min(best,totalDiff-num+abs(nums2[i]-nums1[idx]))
+        return best%mod
+
+if __name__=="__main__":
+    nums1 = [1,7,5]
+    nums2 = [2,3,5]
+    print(Solution().minAbsoluteSumDiff(nums1=nums1, nums2=nums2))
--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print(100000/0.01)
+import bisect
+from typing import List
+class Solution:
+    def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
+        mod = 10**9 + 7
+        absdiffsum, absdiff = 0, []
+        mi = float('inf')
+        n = len(nums1)
+        for i in range(n):
+            diff = abs(nums1[i] - nums2[i])
+            absdiffsum += diff
+            absdiff.append(diff)
+        nums1.sort()
+        for num, diff in zip(nums2, absdiff):
+            idx = bisect.bisect_left(nums1, num)
+            if idx > 0:
+                mi = min(mi, absdiffsum - diff + abs(num - nums1[idx-1]))
+            if idx < n:
+                mi = min(mi, absdiffsum - diff + abs(num - nums1[idx]))
+        return mi % mod
+
+
+if __name__=="__main__":
+    nums1 = [1,10,4,4,2,7]
+    nums2 = [9,3,5,1,7,4]
+    print(Solution().minAbsoluteSumDiff(nums1=nums1, nums2=nums2))
