090b168b90028d922c72ef61850cd6be0f314db2
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-07 20:10:16 +0530

    Till 01/07/2023 20:10:16

--- 240_Search_a_2D_Matrix_II.py
+++ 240_Search_a_2D_Matrix_II.py
@@ -1,3 +1,4 @@
+# VVI
 from typing import List
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
@@ -15,3 +16,4 @@ if __name__=="__main__":
     matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
     target=12
     print(Solution().searchMatrix(matrix=matrix,target=target))
+
