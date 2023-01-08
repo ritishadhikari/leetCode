943e780d75cee409798e107d10af962995f3ca86
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-08 10:37:31 +0530

    Till 01/08/2023 10:37:31

--- 240_Search_a_2D_Matrix_II.py
+++ 240_Search_a_2D_Matrix_II.py
@@ -13,7 +13,11 @@ class Solution:
         return False
 
 if __name__=="__main__":
-    matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+    matrix = [[1,4,7,11,15],
+              [2,5,8,12,19],
+              [3,6,9,16,22],
+              [10,13,14,17,24],
+              [18,21,23,26,30]]
     target=12
     print(Solution().searchMatrix(matrix=matrix,target=target))
 
