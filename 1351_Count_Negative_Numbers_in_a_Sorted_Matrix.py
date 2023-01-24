59156a5a25373cadb4a51d7bb89910db1d851d25
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-24 22:22:47 +0530

    Till 01/24/2023 22:22:46

---
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+            l=0
+            r=len(grid)-1
+            c=0
+            while r>=0 and c<len(grid[0]):
+                if grid[r][c]<0: 
+                    l+=len(grid[0])-c
+                    r-=1
+                else: c+=1
+            return l
+
+if __name__=="__main__":
+
+    grid= \
+    [
+        [ 4,  3,  2, -1],
+        [ 3,  2,  1, -2],
+        [ 1, -2, -3, -4],
+        [ 2, -3, -4, -5]
+    ]	
+
+    print(Solution().countNegatives(grid=grid))
