70c31ae380be5c15cbb8f257741364cd659c0613
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-25 18:38:00 +0530

    Till 01/25/2023 18:38:00

--- 153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -4,26 +4,16 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         l=0
         r=len(nums)-1
-        m=(l+r)//2
         while l<=r:
-            if nums[l]==nums[m] and nums[m]==nums[r]:
-                return nums[m]
-            elif nums[m]>nums[l] and nums[m]<nums[r]:  # normal ascending matrix
-                r=m-1
-            elif nums[m]<nums[l] and nums[m]<nums[r]:  # min in the left half
-                r=m
-            elif nums[m]>nums[l] and nums[m]>nums[r]:  # min in the right half
-                l=m
-            elif nums[m]==nums[l] and nums[m]>nums[r]:  # min is to the right of left
-                l=r
-            elif nums[m]==nums[l] and nums[m]<nums[r]:  # min is to the left of right
-                r=l 
-            elif nums[m]>nums[l] and nums[m]==nums[r]: # min is to the left of right
-                r=l
-            elif nums[m]<nums[l] and nums[m]==nums[r]: # min is to the right of left
-                l=r
             m=(l+r)//2
+            if nums[m]>=nums[l] and nums[m]<=nums[r]:  # normal ascending matrix
+                r=m-1
+            elif nums[m]<nums[l] and nums[m]<nums[r]:  # min in the left half [12,11,7,8,9], [6,7,1,2,3,4,5]
+                r=m
+            elif nums[m]>=nums[l] and nums[m]>=nums[r]:  # min in the right half [4,5,6,7,0,1,2], [3,4,5,6,7,1,2]
+                l=m+1
+        return nums[m]
 
 if __name__=="__main__":
-    nums=[0,1]
+    nums=[4,5,6,7,0,1,2]
     print(Solution().findMin(nums=nums))
