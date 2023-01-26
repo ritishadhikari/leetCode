cd2db1975a4113722104fc5c63177641527857f1
Ritish Adhikari <ritishadhikari@gmail.com>
2023-01-26 23:08:47 +0530

    Till 01/26/2023 23:08:47

--- 2089_Find_Target_Indices_After_Sorting_Array.py
+++ 2089_Find_Target_Indices_After_Sorting_Array.py
@@ -5,6 +5,9 @@ class Solution:
         
         
         def getRemainingIndices(startingIndex):
+            '''
+                Using Linear Search to find the same next values
+            '''
             lst=[]
             while startingIndex<len(nums):
                 if nums[startingIndex]==target: 
@@ -13,16 +16,29 @@ class Solution:
                 else: break
             return lst
         
+        def getRemainingIndices(startingIndex):
+            '''
+                Using Binary Search to find the same next Values
+            '''
+            lst=[]
+            if startingIndex<len(nums) and nums[startingIndex]==target:
+                l=startingIndex
+                r=len(nums)-1
+                while l<=r:
+                    m=(l+r)//2
+                    if nums[m]==target: 
+                        lst+=list(range(l,m+1))
+                        l=m+1
+                    else:r=m-1
+            return lst
+        
         nums.sort()
         
         index=bisect_left(a=nums,x=target)
         return getRemainingIndices(startingIndex=index)
         
 
-
-
-
 if __name__=="__main__":
-    nums=[1,2,5,2,3]
-    target=5
+    nums=[2,1,2,5,2,3,2]
+    target=2
     print(Solution().targetIndices(nums=nums,target=target))
