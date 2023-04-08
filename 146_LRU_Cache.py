0721a714b138623de06508cc30b7cfd19da03c8d
Ritish Adhikari <ritishadhikari@gmail.com>
2023-04-08 18:39:10 +0530

    Till 04/08/2023 18:39:09

---
+++ 146_LRU_Cache.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dct=OrderedDict()
+        self.capacity=capacity
+
+    def get(self, key: int) -> int:
+        if key in self.dct:
+            self.dct.move_to_end(key=key,last=True)
+            return self.dct[key]
+        else:
+            return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dct:
+            self.dct.move_to_end(key=key,last=True)
+        self.dct[key]=value
+        if len(self.dct)>self.capacity:
+            self.dct.pop(next(iter(self.dct)))
+
+if __name__=="__main__":
+    obj=LRUCache(capacity=2)
+    print(obj.put(key=1,value=1))
+    print(obj.put(key=2,value=2))
+    print(obj.get(key=1))
+    print(obj.put(key=3,value=3))
+    print(obj.get(key=2))
+    print(obj.put(key=4,value=4))
+    print(obj.get(key=1))
+    print(obj.get(key=3))
+    print(obj.get(key=4))
--- test.py
+++ test.py
@@ -1,51 +1,11 @@
-from typing import List
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        # Edge case: character in word does not exist in board
-        for c in word:
-            if c not in set().union(*[set(row) for row in board]):
-                return False        
-        
-        # Initialize DFS stack. Item = (curr x, curr y, letters so far, visited set)
-        stack = []
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    stack.append((i, j, 1, {(i, j)}))
-                    
-        # DFS
-        while stack:
-            x, y, ct, visited = stack.pop()
-            
-            # Solution found
-            if ct == len(word):
-                return True
-            
-            # Check up, down, left, right neighbors
-            for newX, newY in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-                if 0 <= newX and newX < len(board) and 0 <= newY and newY < len(board[0]):
-                    if board[newX][newY] == word[ct] and (newX, newY) not in visited:
-                        # If neighbor is in range and matches next letter and not visited,
-                        # Add neighbor onto visited and add onto stack
-                        newVisited = set(visited)
-                        newVisited.add((newX, newY))
-                        stack.append((newX, newY, ct + 1, newVisited))
-                        
-        # No solution found
-        return False
-    
+from collections import OrderedDict
 
-if __name__=="__main__":
-    board = [
-        ["A","B","C","E"],
-        ["S","F","C","S"],
-        ["A","D","E","E"]
-        ]
-    
-    board=[
-            ["C","B","C"],
-            ["C","D","C"],
-            ["B","B","D"]
-        ]
-    word = "BCCB"
-    print(Solution().exist(board=board,word=word))
+dct=OrderedDict()
+
+dct['a']=2
+dct['b']=3
+
+v=next(iter(dct))
+
+print(dct.pop(v))
+print(dct)
