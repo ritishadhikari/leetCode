7d6f52845d8193751830ced475b2ad1f8b08e08f
Ritish Adhikari <ritishadhikari@gmail.com>
2023-04-15 20:27:11 +0530

    Till 04/15/2023 20:27:11

---
+++ 334_Increasing_Triplet_Subsequence.py
@@ -0,0 +1,20 @@
+from typing import List
+from math import inf
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first=inf
+        second=inf
+        for n in nums:
+            if n<=first:
+                first=n
+            elif n<=second:
+                second=n
+            else:
+                return True
+        return False
+
+            
+
+if __name__=="__main__":
+    nums=[5, 1, 5, 5,3,4] 
+    print(Solution().increasingTriplet(nums=nums))
