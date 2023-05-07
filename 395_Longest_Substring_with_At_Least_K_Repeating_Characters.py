dc5c36a468304c2e5cb692d367283bc65d6f8ffa
Ritish Adhikari <ritishadhikari@gmail.com>
2023-05-07 21:22:49 +0530

    Till 05/07/2023 21:22:48

---
+++ 395_Longest_Substring_with_At_Least_K_Repeating_Characters.py
@@ -0,0 +1,43 @@
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+    
+        uniqueChars=set(s)
+        totalChars=len(s)
+        res=0
+
+        for charNo in range(1,len(uniqueChars)+1):
+            i=0
+            j=0
+            tracker={}
+
+            while j<totalChars:
+                if s[j] not in tracker:
+                    tracker[s[j]]=1
+                else:
+                    tracker[s[j]]+=1
+        
+                if len(tracker)<charNo:
+                    j+=1
+                    continue
+                elif len(tracker)==charNo:
+                    count=0
+                    for key,value in tracker.items():
+                        if value>=k:
+                            count+=1
+                            if count==charNo:
+                                res=max(res,j-i+1)
+                else:
+                        while len(tracker)>charNo:
+                            tracker[s[i]]-=1
+                            if tracker[s[i]]==0:
+                                tracker.pop(s[i])
+                            i+=1
+
+                j+=1
+
+        return res
+
+if __name__=="__main__":
+    s = "bchhbbdefghiaaacb"
+    k=3
+    print(Solution().longestSubstring(s=s,k=k))
