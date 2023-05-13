eb87d3c691f7cc9d964f94ebe58c8cca75b30e1f
Ritish Adhikari <ritishadhikari@gmail.com>
2023-05-13 18:03:00 +0530

    Till 05/13/2023 18:02:59

---
+++ 134_Gas_Station.py
@@ -0,0 +1,20 @@
+# VVI
+from typing import List
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas)<sum(cost):
+            return -1
+        else:
+            start=0
+            runningSum=0
+            for i in range(len(gas)):
+                runningSum=runningSum+gas[i]-cost[i]
+                if runningSum<0:
+                    start=i+1
+                    runningSum=0
+            return start
+
+if __name__=="__main__":
+    gas = [7,1,0,11,4]
+    cost = [5,9,1,2,5]
+    print(Solution().canCompleteCircuit(gas=gas, cost=cost))
--- test.py
+++ test.py
@@ -1,29 +1,17 @@
-from collections import deque
+from typing import List
 class Solution:
-    def canFinish(self, n, pres):
-        ind = [[] for _ in range(n)]  # indegree
-        oud = [0] * n  # outdegree
-        for p in pres:
-            oud[p[0]] += 1
-            ind[p[1]].append(p[0])
-        dq = deque()
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n, total_surplus, surplus, start = len(gas), 0, 0, 0
+        
         for i in range(n):
-            if oud[i] == 0:
-                dq.append(i)
-        k = 0
-        while dq:
-            x = dq.popleft()
-            k += 1
-            for i in ind[x]:
-                oud[i] -= 1
-                if oud[i] == 0:
-                    dq.append(i)
-        return k == n
-
+            total_surplus += gas[i] - cost[i]
+            surplus += gas[i] - cost[i]
+            if surplus < 0:
+                surplus = 0
+                start = i + 1
+        return -1 if (total_surplus < 0) else start
 
 if __name__=="__main__":
-    # n=5
-    # pres=[[2,1],[0,1],[1,4],[2,3],[2,4]]
-    n=2
-    pres=[[1,0],[0,1]]
-    print(Solution().canFinish(n=n,pres=pres))
+    gas = [1,2,3,4,5]
+    cost = [3,4,5,1,2]
+    print(Solution().canCompleteCircuit(gas=gas,cost=cost))
