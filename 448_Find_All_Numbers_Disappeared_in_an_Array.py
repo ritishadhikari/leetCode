8eebf19cbe904d6772a4dadbd5d38e0b0223f0b3
Ritish Adhikari <ritishadhikari@gmail.com>
2023-05-26 21:18:54 +0530

    Till 05/26/2023 21:18:53

---
+++ 448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        for i in range(len(nums)):
+            absNum=abs(nums[i])
+            index=absNum-1
+            if nums[index]>0:
+                nums[index]=nums[index]*-1
+        lst=[]
+        for ind, num in enumerate(nums):
+            if num>0:
+                lst.append(ind+1)
+        
+        return lst
+        
+    
+
+if __name__=="__main__":
+    nums=[1,1]
+    print(Solution().findDisappearedNumbers(nums=nums))
