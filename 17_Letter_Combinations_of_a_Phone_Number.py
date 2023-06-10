71ba8456cb8304555c88eccc9896bd2d81ece77c
Ritish Adhikari <ritishadhikari@gmail.com>
2023-06-11 01:40:07 +0530

    Till 06/11/2023 01:40:06

---
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,21 @@
+from typing import List
+from itertools import product
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits=="":
+            return []
+        else:
+            traceList=[]
+            for s in digits:
+                
+                if s in ["1","2","3","4","5","6"]:
+                    traceList.append([chr(97+((int(s)-2)*3+k)) for k in range(3)])
+                elif s in ["7"]: traceList.append(["p","q","r","s"])
+                elif s in ["8"]: traceList.append(["t","u","v"])
+                elif s in ["9"]: traceList.append(["w","x","y","z"])
+
+        return [''.join(m) for m in list(product(*traceList))]
+
+if __name__=="__main__":
+    digits="22"
+    print(Solution().letterCombinations(digits=digits))
--- test.py
+++ test.py
@@ -1,20 +1,2 @@
-from typing import List
-class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        n=len(gas) 
-        total_surplus=0 
-        surplus=0
-        start = 0
-        
-        for i in range(n):
-            total_surplus += gas[i] - cost[i]
-            surplus += gas[i] - cost[i]
-            if surplus < 0:
-                surplus = 0
-                start = i + 1
-        return -1 if (total_surplus < 0) else start
-
-if __name__=="__main__":
-    gas = [1,2,3,4,5]
-    cost = [3,4,5,1,2]
-    print(Solution().canCompleteCircuit(gas=gas,cost=cost))
+print(ord('c'))
+print(chr(99))
