20660a3d2cf73ca1503fb0d976a4b4bfd4df2093
ritishadhikari <ritishadhikari@gmail.com>
2023-11-04 14:23:37 +0530

    Till 11/04/2023 14:23:37

--- 180_Consecutive_Numbers.py
+++ 180_Consecutive_Numbers.py
@@ -8,7 +8,7 @@ def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
             ans.append(num)
         secondLast,last=last,num
     
-    return pd.DataFrame(data=[[j] for j in ans],columns=["ConsecutiveNums"])
+    return pd.DataFrame(data=[[j] for j in set(ans)],columns=["ConsecutiveNums"])
 
 if __name__=="__main__":
     logs=pd.DataFrame(data=[[1,1],[2,1],[3,1],
