32b140205cafaf95660580ae762def5a3db83b3b
ritishadhikari <ritishadhikari@gmail.com>
2024-01-19 20:04:09 +0530

    Till 01/19/2024 20:04:08

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -1,7 +1,24 @@
 # VVI
 from typing import List
+from collections import defaultdict
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        forward=defaultdict(lambda:1)
+        back=defaultdict(lambda:1)
+        fm,bm=1,1
+        for i in range(len(nums)):
+            b=(i*-1)-1
+            fm*=nums[i]
+            bm*=nums[b]
+            forward[i],back[b]=fm,bm
+        answer=[]
+        for i in range(len(nums)):
+            fmi,bmi=i-1,(len(nums)*-1)+i+1
+            answer.append(forward[fmi]*back[bmi])
+        return answer
+        
+        
+        
         answer=[1]*len(nums)
         suffix=1
         prefix=1
