b4a9e1cf57ff5bf0589a82b8b5af2a61f330f37a
ritishadhikari <ritishadhikari@gmail.com>
2024-02-05 20:58:40 +0530

    Till 02/05/2024 20:58:39

--- 540_Single_Element_in_a_Sorted_Array.py
+++ 540_Single_Element_in_a_Sorted_Array.py
@@ -2,7 +2,7 @@ from typing import List
 class Solution():
     '''
     the single number will always be at the even index
-    # expectation is the odd index and the subsequent even index is same
+    # expectation is the even index and the subsequent odd index is same
     # if it is not same then the single number exists atleast prior to the middle number
     '''
     def singleNonDuplicate(self, nums: List[int]) -> int:
@@ -10,17 +10,19 @@ class Solution():
         h=len(nums)-1
         while l<=h:
             m=(l+h)//2
-            # additional check since we are dealing with the next or the prior index number
-            if m==len(nums)-1: return nums[m]  
+            # when we have actually arrived at the number with the single appearance
+            if l==h: return nums[m]  
             #   odd condition                      even condition
             if (m%2==1 and nums[m]==nums[m-1]) or (m%2==0 and nums[m]==nums[m+1]):
                 l=m+1
             else:
-                if l==m==h or m%2==1:  h=m-1
+                # odd condition 
+                if m%2==1:  h=m-1
+                # even condition
                 else: h=m
         return nums[m]
 
 
 if __name__=="__main__":
-    nums = [1]
+    nums = [1,1,2,3,3,5,5]
     print(Solution().singleNonDuplicate(nums=nums))
