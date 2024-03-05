550325fd4ce908127dd47f80c2989c2f70786cca
ritishadhikari <ritishadhikari@gmail.com>
2024-03-05 19:44:06 +0530

    Till

--- 1845_Seat_Reservation_Manager.py
+++ 1845_Seat_Reservation_Manager.py
@@ -3,13 +3,18 @@ class SeatManager:
     
     def __init__(self,n:int):
         self.seat=[i for i in range(1,n+1)]
+        self.unreservedSeat=set(self.seat)
         heapq.heapify(self.seat)
 
     def reserve(self) -> int:
-        return heapq.heappop(self.seat)        
+        seatToReserve=heapq.heappop(self.seat)   
+        self.unreservedSeat.remove(seatToReserve)
+        return seatToReserve     
 
     def unreserve(self,seatNumber: int) -> None:
-        heapq.heappush(self.seat,seatNumber)
+        if seatNumber not in self.unreservedSeat:
+            heapq.heappush(self.seat,seatNumber)
+            self.unreservedSeat.add(seatNumber)
 
 if __name__=="__main__":
     # obj=SeatManager(n=100)
@@ -25,7 +30,7 @@ if __name__=="__main__":
     obj=SeatManager(n=100)
     print(obj.reserve())
     print(obj.reserve())
-    print(obj.unreserve(2))
+    print(obj.unreserve(3))
     print(obj.reserve())
     print(obj.reserve())
     print(obj.reserve())
