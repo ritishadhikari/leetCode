0c438be6fa47a61dcd45b853bdce7711b83607bc
ritishadhikari <ritishadhikari@gmail.com>
2024-04-24 20:38:22 +0530

    Till 04/24/2024 20:38:21

--- 1823_Find_the_Winner_of _the_Circular_Game.py
+++ 1823_Find_the_Winner_of _the_Circular_Game.py
@@ -2,10 +2,11 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         lst=[i for i in range(1,n+1)]
         pos=0
+        length=len(lst)
         while len(lst)>1:
-            length=len(lst)
             pos=(pos+(k%length)-1)%length
             lst.pop(pos)
+            length-=1
         return lst[0]
 
 if __name__=="__main__":
