e74a31a441735ac25108790945a08f3a56a25a57
ritishadhikari <ritishadhikari@gmail.com>
2024-05-29 22:09:50 +0530

    Till 05/29/2024 22:09:50

--- test.py
+++ test.py
@@ -1,50 +1,15 @@
-from typing import Optional,List
-from itertools import permutations
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+import collections
 class Solution:
-     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        lists=permutations([i for i in range(1,n+1)])
-
-        uniqueTrees=set()
-        uniqueArch=set()
-        for lst in lists:
-
-            for i,val in enumerate(lst):
-                if i==0:
-                    tree=TreeNode(val=val)
-                    arch={'v':val,'l':{},'r':{}}
-                else:
-                    realTree=tree
-                    realArch=arch
-                    while tree:
-                        if val<tree.val:
-                            if tree.left:
-                                tree=tree.left
-                                arch=arch['l']
-                            else:
-                                tree.left=TreeNode(val=val)
-                                arch['l']={'v':val,'l':{},'r':{}}
-                                break
-                        else:
-                            if tree.right:
-                                tree=tree.right
-                                arch=arch['r']
-                            else:
-                                tree.right=TreeNode(val=val)
-                                arch['r']={'v':val,'l':{},'r':{}}
-                                break
-                    tree=realTree
-                    arch=realArch
-            if str(arch) not in uniqueArch:
-                uniqueArch.add(str(arch))
-                uniqueTrees.add(tree)
-
-        return uniqueTrees
-     
+    def getHappyString(self, n: int, k: int) -> str:
+        nextLetter = {'a': 'bc', 'b': 'ac', 'c': 'ab'} 
+        q = collections.deque(['a', 'b', 'c'])
+        while len(q[0]) != n:
+            u = q.popleft()    
+            for v in nextLetter[u[-1]]:
+                q.append(u + v)
+        return q[k - 1] if len(q) >= k else '' 
+    
 if __name__=="__main__":
     n=3
-    print(Solution().generateTrees(n=n))
+    k=9
+    print(Solution().getHappyString(n=n,k=k))
