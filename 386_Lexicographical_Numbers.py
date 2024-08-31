cdb2ebbdda65827b027428203170dcac11e7e92e
ritishadhikari <ritishadhikari@gmail.com>
2024-08-31 21:13:44 +0530

    Till 08/31/2024 21:13:44

--- 386_Lexicographical_Numbers.py
+++ 386_Lexicographical_Numbers.py
@@ -3,7 +3,7 @@ class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         ns=sorted([str(i+1) for i in range(n)])
         return [int(number) for number in ns]
-        return ns
+       
 
 
 if __name__=="__main__":
