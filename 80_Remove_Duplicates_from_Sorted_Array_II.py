57a57e6f2896b07d8362853cdd48e08059bfbddb
ritishadhikari <ritishadhikari@gmail.com>
2024-10-09 19:34:44 +0530

    Till 10/09/2024 19:34:44

---
+++ 80_Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import Counter,OrderedDict
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        freq=OrderedDict(Counter(nums))
+        # extra=0
+        # index=0
+        # for val, count in freq.items():
+        #     if count<=2:
+        #         nums[index:index+count]=[val]*count
+        #         index+=count
+        #     else:
+        #         nums[index:index+2]=[val]*2
+        #         index+=2
+        #         extra+=count-2
+        # return len(nums[:index])
+        count=0
+        for val, occ in freq.items():
+            if occ>2:
+                count+=2
+            else:
+                count+=occ
+        return count
+
+
+if __name__=="__main__":
+    nums =  [0,0,1,1,1,1,2,3,3]
+    print(Solution().removeDuplicates(nums=nums))
