493c42f8eb92941d1fc16168aafc37d19ded9c25
ritishadhikari <ritishadhikari@gmail.com>
2024-10-21 14:28:49 +0530

    Till 10/21/2024 14:28:49

---
+++ 585_Investments_in_2016.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from collections import Counter
+def find_investments(insurance: pd.DataFrame) -> pd.DataFrame:
+    answer=0
+    count=Counter(insurance['tiv_2015'].values.tolist())
+    for i,val in insurance.iterrows():
+        tiv2015=val['tiv_2015']
+        if count[tiv2015]>1:  # First Condition is satisfied
+            lat=val['lat']
+            lon=val['lon']
+            dfLatLong=insurance.loc[
+                (insurance['pid']!=val['pid']) &
+                (insurance['lat']==lat) &
+                (insurance['lon']==lon)
+                ]
+            if len(dfLatLong)==0:  # Second Condition is Satisfied
+                answer+=val['tiv_2016']
+
+    return pd.DataFrame(data=[answer],columns=['tiv_2016'])  
+
+if __name__=="__main__":
+    insurance=pd.DataFrame(data=zip(
+        [1,2,3,4],
+        [10,20,10,10],
+        [5,20,30,40],
+        [10,20,20,40],
+        [10,20,20,40]
+    ),columns=["pid","tiv_2015","tiv_2016","lat","lon"])
+    print(find_investments(insurance=insurance))
