0f0b9f47b6618ca21c7baadb4666df7eec64c05c
ritishadhikari <ritishadhikari@gmail.com>
2024-11-20 19:12:58 +0530

    Till 11/20/2024 19:12:57

---
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
@@ -0,0 +1,18 @@
+from typing import List
+from math import inf
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cd,sell,hold=0,0,-inf
+        for price in prices:
+            last_cd,last_sell,last_hold=cd,sell,hold
+            cd=max(last_cd,last_sell)
+            # You can only sell the stock from your last hold price
+            sell=last_hold+price
+            # You can only decide to buy/hold the stock comparing one
+            # previous day of cool down savings
+            hold=max(last_hold,last_cd-price)
+        return max(cd,sell)
+
+if __name__=="__main__":
+    prices = [1,2,3,0,2]
+    print(Solution().maxProfit(prices=prices))
